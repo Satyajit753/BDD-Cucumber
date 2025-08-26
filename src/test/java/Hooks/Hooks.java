4456980aa8fc8534132502662fc9abee2b0eb011
Satyajit Pradhan <bapisatyajit02@gmail.com>
2025-08-26 12:08:55 +0530

    initial commit

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -1,74 +1,34 @@
-//package Hooks;
-// 
-//import org.openqa.selenium.OutputType;
-//
-//import org.openqa.selenium.TakesScreenshot;
-//
-//import org.openqa.selenium.WebDriver;
-// 
-//import io.cucumber.java.After;
-//
-//import io.cucumber.java.Before;
-//
-//import io.cucumber.java.Scenario;
-//
-//import utils.DriverFactory;
-// 
-//public class Hooks {
-//	// Inject WebDriver from DriverFactory
-//	 @Before
-//	 public void setUp() {
-//		 DriverFactory.getDriver();
-//		 System.out.println("Browser launched before scenario");
-//	 }
-//	 @After
-//	 public void tearDown(Scenario scenario) {
-//		 WebDriver driver = DriverFactory.getDriver();
-//		 if(scenario.isFailed()) {
-//			  byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-//	          scenario.attach(screenshot, "image/png", "Failed Screenshot"); 
-//		 }
-//		 DriverFactory.quitDriver();
-//		 System.out.println("Browser closed after the scenario");
-//	 }
-//}
-//
-// 
 package Hooks;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import io.cucumber.java.AfterAll;
-import io.cucumber.java.BeforeAll;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import utils.DriverFactory;
 
 public class Hooks {
 
-    private static WebDriver driver;
+    private WebDriver driver;
 
-    @BeforeAll
-    public static void setUp() {
+    @Before
+    public void setUp() {
         driver = DriverFactory.getDriver();
-        System.out.println("Browser launched once before all scenarios");
+        System.out.println("Browser launched before scenario");
     }
 
-    @AfterAll
-    public static void tearDown() {
+    @After
+    public void tearDown() {
         DriverFactory.quitDriver();
-        System.out.println("Browser closed after all scenarios");
+        System.out.println("Browser closed after scenario");
     }
 
-    @io.cucumber.java.After
+    @After
     public void takeScreenshotOnFailure(Scenario scenario) {
         if (scenario.isFailed() && driver != null) {
             byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
             scenario.attach(screenshot, "image/png", "Failed Screenshot");
         }
-    }
-
-    public static WebDriver getDriver() {
-        return driver;  
     }
 }
--- src/test/java/TestRunner/MyTestRunner.java
+++ src/test/java/TestRunner/MyTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = {"src/test/resources/Features"},
+    features = {"src/test/resources/Features/Lab2.feature"},
     glue = {"StepDefinitions"},
     plugin = {"pretty", "html:target/cucumber-reports.html"},
     monochrome = true
