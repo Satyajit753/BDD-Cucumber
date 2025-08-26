8b5772e8dc1792e214c7bdcae3c690fd1a6cae6a
Satyajit753 <76035087+Satyajit753@users.noreply.github.com>
2025-08-26 15:47:44 +0530

    LabSession2_step.java

--- src/test/java/StepDefinitions/LabSession2_step.java
+++ src/test/java/StepDefinitions/LabSession2_step.java
@@ -1,11 +1,15 @@
 package StepDefinitions;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.en.*;
 import utils.DriverFactory;
@@ -24,49 +28,49 @@ public class LabSession2_step {
     @When("the user enters {string} as first name")
     public void the_user_enters_as_first_name(String firstName) throws InterruptedException {
         driver.findElement(By.name("firstName")).sendKeys(firstName);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user enters {string} as last name")
     public void the_user_enters_as_last_name(String lastName) throws InterruptedException {
         driver.findElement(By.name("lastName")).sendKeys(lastName);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user enters {string} as phone number")
     public void the_user_enters_as_phone_number(String phone) throws InterruptedException {
         driver.findElement(By.name("phone")).sendKeys(phone);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user provides {string} as email")
     public void the_user_provides_as_email(String email) throws InterruptedException {
-        driver.findElement(By.id("userName")).sendKeys(email);  // fixed locator
-        Thread.sleep(1000);
+        driver.findElement(By.id("userName")).sendKeys(email);
+        Thread.sleep(500);
     }
 
     @When("the user enters {string} as street address")
     public void the_user_enters_as_street_address(String address) throws InterruptedException {
         driver.findElement(By.name("address1")).sendKeys(address);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user enters {string} as city")
     public void the_user_enters_as_city(String city) throws InterruptedException {
         driver.findElement(By.name("city")).sendKeys(city);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user enters {string} as state")
     public void the_user_enters_as_state(String state) throws InterruptedException {
         driver.findElement(By.name("state")).sendKeys(state);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user enters {string} as postal code")
     public void the_user_enters_as_postal_code(String postalCode) throws InterruptedException {
         driver.findElement(By.name("postalCode")).sendKeys(postalCode);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user selects {string} as country")
@@ -74,25 +78,25 @@ public class LabSession2_step {
         WebElement countryDropdown = driver.findElement(By.name("country"));
         Select select = new Select(countryDropdown);
         select.selectByVisibleText(country);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user enters {string} as username")
     public void the_user_enters_as_username(String username) throws InterruptedException {
         driver.findElement(By.id("email")).sendKeys(username);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user enters {string} as password")
     public void the_user_enters_as_password(String password) throws InterruptedException {
         driver.findElement(By.name("password")).sendKeys(password);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user enters {string} as confirm password")
     public void the_user_enters_as_confirm_password(String confirmPassword) throws InterruptedException {
         driver.findElement(By.name("confirmPassword")).sendKeys(confirmPassword);
-        Thread.sleep(1000);
+        Thread.sleep(500);
     }
 
     @When("the user clicks on registration submit button")
@@ -102,21 +106,21 @@ public class LabSession2_step {
             submitBtn.click();
             System.out.println("Submit button clicked normally");
         } catch (ElementClickInterceptedException e) {
-            // fallback to JavaScript click
             ((JavascriptExecutor) driver).executeScript("arguments[0].click();", submitBtn);
             System.out.println("Submit button clicked using JavaScript due to interception");
         }
         Thread.sleep(1000);
     }
 
-    /*@Then("the registration should be successful")
-    public void the_registration_should_be_successful() throws InterruptedException {
-        String successMsg = driver.findElement(By.xpath("//font[contains(text(),'Thank you for registering')]")).getText();
-        if (successMsg.contains("Thank you for registering")) {
-            System.out.println("Registration successful");
-        } else {
-            System.out.println("Registration failed");
+    @Then("the registration should be successful")
+    public void the_registration_should_be_successful() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            WebElement msg = wait.until(ExpectedConditions.presenceOfElementLocated(
+                    By.xpath("//*[contains(text(),'Thank you for registering')]")));
+            System.out.println("Registration successful: " + msg.getText());
+        } catch (Exception e) {
+            System.out.println("Registration failed - success message not found");
         }
-        Thread.sleep(1000);*/
     }
 }
